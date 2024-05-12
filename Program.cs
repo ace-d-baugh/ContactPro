d7e83643cb02769d415d0faccb54ea98af4c3884
Ace Baugh <ace.d.baugh@gmail.com>
2024-05-11 21:55:28 -0400

    Fixed errors. Registration updated

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));

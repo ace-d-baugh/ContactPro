e13ac464060be6e42c1ea4ee9b285dac7c9ec03d
Ace Baugh <ace.d.baugh@gmail.com>
2024-05-19 23:19:50 -0400

    added image service

---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactPro.Services.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<byte[]> ConvertFileToByteArray(IFormFile file);
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}

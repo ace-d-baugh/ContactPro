48276dd79893553808be07222787ee3ded05a5d2
Ace Baugh <ace.d.baugh@gmail.com>
2024-05-22 16:07:10 -0400

    Created DataHelper to help when launched

---
+++ Helpers/DataHelper.cs
@@ -0,0 +1,17 @@
+﻿susing ContactPro.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactPro.Helpers
+{
+    public static class DataHelper
+    {
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            // get an instance of the db application context
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            // migration: equivalent to update-database
+            await dbContextSvc.Database.MigrateAsync();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,11 +5,13 @@ using Microsoft.EntityFrameworkCore;
 using ContactPro.Services;
 using ContactPro.Services.Interfaces;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using ContactPro.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+//var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
@@ -27,6 +29,10 @@ builder.Services.AddScoped<IEmailSender, EmailService>();
 builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
 var app = builder.Build();
+var scope = app.Services.CreateScope();
+
+// Get the database update with the latest migrations
+await DataHelper.ManageDataAsync(scope.ServiceProvider);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

5305f806e2eaf0b901e8a82e935456360f0a126a
Ace Baugh <ace.d.baugh@gmail.com>
2024-05-21 22:33:14 -0400

    Adding model to hold secret email info

---
+++ Models/MailSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactPro.Models
+{
+    public class MailSettings
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public bool UseSsl { get; set; } = true;
+        public string? DisplayName { get; set; }
+        public int Port { get; set; } = 587;
+        public string? Host { get; set; }
+
+    }
+}

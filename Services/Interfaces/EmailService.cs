9e551dcb78dc9af6015a41af6b4444cc4b706bc6
Ace Baugh <ace.d.baugh@gmail.com>
2024-05-21 22:52:09 -0400

    Wiring up Email Settings for connection

---
+++ Services/Interfaces/EmailService.cs
@@ -0,0 +1,65 @@
+﻿using ContactPro.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MailKit.Security;
+using MailKit.Net.Smtp;
+
+
+namespace ContactPro.Services
+{
+    public class EmailService : IEmailSender
+    {
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var emailSender = _mailSettings.Email ?? Environment.GetEnvironmentVariable("Email");
+
+            MimeMessage newEmail = new();
+
+            newEmail.Sender = MailboxAddress.Parse(emailSender);
+
+            // loop over all the email addresses in the email string and add them to the email to list
+            foreach (var emailAddress in email.Split(";"))
+            {
+                newEmail.To.Add(MailboxAddress.Parse(emailAddress));
+            }
+
+            newEmail.Subject = subject;
+
+            BodyBuilder emailBody = new();
+            emailBody.HtmlBody = htmlMessage;
+
+            newEmail.Body = emailBody.ToMessageBody();
+
+
+            // Login into SMTP client
+            using SmtpClient smtpClient = new();
+
+            try
+            {
+                var host = _mailSettings.Host ?? Environment.GetEnvironmentVariable("Host");
+                var port = _mailSettings.Port != 0 ? _mailSettings.Port : int.Parse(Environment.GetEnvironmentVariable("Port")!);
+                var password = _mailSettings.Password ?? Environment.GetEnvironmentVariable("Password");
+
+                await smtpClient.ConnectAsync(host, port, SecureSocketOptions.StartTls);
+                await smtpClient.AuthenticateAsync(emailSender, password);
+
+                await smtpClient.SendAsync(newEmail);
+
+                await smtpClient.DisconnectAsync(true);
+            }
+            catch (Exception ex)
+            {
+                var error = ex.Message;
+                throw;
+            }
+        }
+    }
+}

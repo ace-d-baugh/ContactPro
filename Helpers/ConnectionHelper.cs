4202f40a1c0f26f56dca0c8fa5e12ff4faa5a139
Ace Baugh <ace.d.baugh@gmail.com>
2024-05-22 16:33:31 -0400

    fixes

--- Helpers/ConnectionHelper.cs
+++ Helpers/ConnectionHelper.cs
@@ -2,17 +2,16 @@
 
 namespace ContactPro.Helpers
 {
-    public class ConnectionHelper
+    public static class ConnectionHelper
     {
         public static string GetConnectionString(IConfiguration configuration)
         {
-            var connectionString = configuration.GetSection("pgSettings")["pgConnection"];
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
             var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
             return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
         }
 
-        // Build a connection string from the environment. i.e. Railway
+        //build the connection string from the environment. i.e. Heroku
         private static string BuildConnectionString(string databaseUrl)
         {
             var databaseUri = new Uri(databaseUrl);
